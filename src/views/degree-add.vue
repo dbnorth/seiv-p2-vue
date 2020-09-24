7936d82dd652c497d4f8daf82b22764dcb312685
David North <david.north@C02X30AGJGH7.local>
2020-09-23 22:03:30 -0500

    add Degree views

---
+++ src/views/degree-add.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <H1>Degree Add</H1>
+                <h4>{{ message }}</h4>
+                <v-form>
+                    <v-text-field label="Dept" v-model="degree.dept" />
+                    <v-text-field label="Degree" v-model="degree.description" />
+                    <v-row justify="center">
+                        <v-col col="3"> </v-col>
+                        <v-col col="2">
+                            <v-btn color="success" @click="addDegree()"
+                                >Add</v-btn
+                            >
+                        </v-col>
+                        <v-col col="2">
+                            <v-btn color="info" @click="cancel()">Cancel</v-btn>
+                        </v-col>
+                        <v-col col="3"> </v-col>
+                    </v-row>
+                </v-form>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import DegreeServices from '@/services/DegreeServices.js';
+
+export default {
+    data() {
+        return {
+            degree: {},
+            message: 'Enter data and click Add',
+        };
+    },
+
+    methods: {
+        addDegree() {
+            DegreeServices.addDegree(this.degree)
+                .then(() => {
+                    this.$router.push({ name: 'degreelist' });
+                })
+                .catch(error => {
+                    this.message = error.response.data.message;
+                });
+        },
+        cancel() {
+            this.$router.push({ name: 'degreelist' });
+        },
+    },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/degree-edit.vue
@@ -0,0 +1,82 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <h1>Degree Edit</h1>
+                <h4>{{ message }}</h4>
+                <v-form>
+                    <v-text-field label="Dept" v-model="degree.dept" />
+                    <v-text-field label="Degree" v-model="degree.description" />
+                    <v-row justify="center">
+                        <v-col col="2"> </v-col>
+                        <v-col col="2">
+                            <v-btn color="success" @click="saveDegree()"
+                                >Save</v-btn
+                            >
+                        </v-col>
+                        <v-col col="2">
+                            <v-btn color="warning" @click="deleteDegree()"
+                                >Delete</v-btn
+                            >
+                        </v-col>
+                        <v-col col="2">
+                            <v-btn color="info" @click="cancel()">Cancel</v-btn>
+                        </v-col>
+                        <v-col col="2"> </v-col>
+                    </v-row>
+                </v-form>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import DegreeServices from '@/services/DegreeServices.js';
+
+export default {
+    components: {},
+    props: ['id'],
+
+    data() {
+        return {
+            degree: {},
+            message: 'Make changes to the degree and Save',
+        };
+    },
+    created() {
+        DegreeServices.getDegree(this.id)
+            .then(response => {
+                this.degree = response.data;
+            })
+            .catch(error => {
+                this.message = error.response.data.message;
+            });
+    },
+
+    methods: {
+        saveDegree() {
+            DegreeServices.updateDegree(this.degree)
+                .then(() => {
+                    this.$router.push({ name: 'degreelist' });
+                })
+                .catch(error => {
+                    this.message = error.response.data.message;
+                });
+        },
+        deleteDegree() {
+            DegreeServices.deleteDegree(this.degree.id)
+                .then(() => {
+                    this.$router.push({ name: 'degreelist' });
+                })
+                .catch(error => {
+                    this.message = error.response.data.message;
+                });
+        },
+        cancel() {
+            this.$router.push({ name: 'degreelist' });
+        },
+    },
+};
+</script>
+
+<style></style>
