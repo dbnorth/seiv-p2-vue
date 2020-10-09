e26880a62dbf8e684e16d15895d691972efa62ee
David North <david.north@C02X30AGJGH7.local>
2020-10-09 09:11:41 -0500

    add component

---
+++ src/components/SemesterCourse.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <h2>Semester {{semester}}</h2>
+  <v-data-table
+      :headers="headers"
+      :items="studentCourses"
+      :items-per-page="10"
+      class="elevation - 1"
+      @click:row="selectRow"
+  >
+  </v-data-table>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    studentCourses :[]
+  },
+  data() {
+        return { 
+            semester :{},         
+            headers: [
+                {
+                    text: 'Semester',
+                    align: 'left',
+                    sortable: true,
+                    value: "semester.code",
+                },
+                {
+                    text: 'Course',
+                    value: 'course.number',
+                    align: 'left',
+                    sortable: false,
+                },
+                {
+                    text: 'Grade',
+                    value: 'grade',
+                    align: 'left',
+                    sortable: false,
+                },
+              
+            ],
+            message: 'Enter click on course to edit'
+        };
+    },
+    created (){
+      this.semester = this.studentCourses[0].semester.code;
+    },
+    methods: {
+        selectRow(event) {
+            let id = event.id;
+            this.$router.push({ name: 'studentcourseedit', params: { id: id } });
+        }
+        
+  
+}
+}
+</script>
