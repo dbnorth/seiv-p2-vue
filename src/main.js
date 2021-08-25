8e2835bd0ef526b628d8ac1a6abde129993836bb
David North <david.north@oc.edu>
2021-08-25 00:32:14 -0500

    vutiefy and port updates

--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ import router from './router.js';
 import vuetify from './plugins/vuetify';
 import store from './store/store.js';
 
-
-import GoogleAuth from './config/google_oAuth.js'
+import GoogleAuth from './config/google_oAuth.js';
 
 const gauthOption = {
-  clientId: '344429607870-mmsdb296ne3vcb7popn96mclv3jnhigv.apps.googleusercontent.com',
-  scope: 'profile email',
-  prompt: 'select_account'
-}
+    clientId:
+        '344429607870-mmsdb296ne3vcb7popn96mclv3jnhigv.apps.googleusercontent.com',
+    scope: 'profile email',
+    prompt: 'select_account',
+};
 Vue.use(GoogleAuth, gauthOption);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App),
 }).$mount('#app');
